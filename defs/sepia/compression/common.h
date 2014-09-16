4c43e7ceaafdc0e2ef9ecf6c8f83a6104bd4a9b6
Kai Hugo Hustoft Endresen <kai.endresen@gmail.com>
2014-09-16 18:02:37 +0200

    added preliminary Zlib based compression framework.

---
+++ defs/sepia/compression/common.h
@@ -0,0 +1,34 @@
+#ifndef SEPIA_COMPRESSION_COMMON_H
+#define SEPIA_COMPRESSION_COMMON_H
+#include <zlib.h> // see http://www.zlib.net/
+
+namespace sepia
+{
+namespace compression
+{
+
+class Common
+{
+public:
+    enum class Method {
+        ZLIB,
+        ZLIB_RLE_ONLY,
+        ZLIB_HUFFMAN_ONLY,
+        FFV1,
+        HUFFYUV
+    };
+
+    void setInput( unsigned char* a_input, size_t a_size );
+    void setOutput( unsigned char* a_input, size_t a_maxSize );
+protected:
+    Common();
+    ~Common();
+    z_stream m_stream;
+    Method m_method;
+    int m_level;
+};
+
+}
+}
+
+#endif // SEPIA_COMPRESSION_COMMON_H
