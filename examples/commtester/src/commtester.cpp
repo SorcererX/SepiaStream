a72c55dc30abca4c6ac5f815d217b2bb89ec7bc7
Kai Hugo Hustoft Endresen <kai.endresen@gmail.com>
2015-09-18 17:46:39 +0200

    comm example - added test parameters
    - message count
    - receive count (receiver threads)
    - message delay (delay between each message)

--- examples/commtester/src/commtester.cpp
+++ examples/commtester/src/commtester.cpp
@@ -7,8 +7,23 @@
 #include <chrono>
 
 
-int main()
+int main( int argc, char** argv )
 {
+    int message_count = 0;
+    int receive_count = 0;
+    int message_delay = 0;
+    if( argc == 4 )
+    {
+        message_count = std::stoi( argv[1] );
+        receive_count = std::stoi( argv[2] );
+        message_delay = std::stoi( argv[3] );
+    }
+    else
+    {
+        std::cout << "Usage:" << std::endl;
+        std::cout << argv[0] << "<message count> <receive count> <message delay>" << std::endl;
+        return 1;
+    }
     // initialize Sepia Client
     sepia::comm::init( "CommTester" );
 
@@ -18,7 +33,7 @@ int main()
     receiver.start();
 
     std::vector< ReceiveTester > m_receivers;
-    m_receivers.resize( 50 );
+    m_receivers.resize( receive_count );
 
     for( int i = 0; i < m_receivers.size(); i++ )
     {
@@ -33,12 +48,16 @@ int main()
     std::cout << "Waiting 20 ms after starting receivers before starting send..." << std::endl;
     usleep( 20000 );
     std::cout << "Starting send." << std::endl;
-    for( int i = 0; i < 100000; i++ )
+    for( int i = 0; i < message_count; i++ )
     {
         message.set_seq_no( i );
         now = std::chrono::system_clock::now();
         message.set_time( now.time_since_epoch().count() );
         sepia::comm::Dispatcher< commtester_msgs::Test >::localSend( &message );
+        if( message_delay > 0 )
+        {
+            usleep( 1000 * message_delay );
+        }
     }
     std::cout << "Send completed." << std::endl;
     std::cout << "Waiting 10 ms after sending last message before closing receivers.." << std::endl;
